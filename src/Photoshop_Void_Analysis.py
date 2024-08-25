f3d5936e79a6dee1ec2e7a449fcfc55d3a06684f
ArcticChicken855 <taupe373@gmail.com>
2024-08-25 12:42:23 -0500

    x

--- src/Photoshop_Void_Analysis.py
+++ src/Photoshop_Void_Analysis.py
@@ -3,12 +3,16 @@ import cv2
 import numpy as np
 import openpyxl
 
-def calculate_void_percent(full_image):
+def calculate_void_percent(full_image, cut):
     """
     Calculate the percentage of pixels in the image that are red.
     """
+    # splice up the image
+    cut_length = round(max(full_image.shape) * cut)
+    image = full_image[cut_length:full_image.shape[1] - cut_length, cut_length:full_image.shape[0] - cut_length]
+
     # Convert the image from BGR to HSV color space
-    hsv_image = cv2.cvtColor(full_image, cv2.COLOR_BGR2HSV)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # Define the lower and upper bounds for red in HSV
     # Red can span over two ranges in the HSV space
@@ -28,7 +32,7 @@ def calculate_void_percent(full_image):
     red_pixel_count = np.sum(red_mask > 0)
 
     # Calculate the total number of pixels in the image
-    total_pixels = full_image.shape[0] * full_image.shape[1]
+    total_pixels = image.shape[0] * image.shape[1]
 
     # Calculate the percentage of red pixels
     percentage_red = (red_pixel_count / total_pixels) * 100
@@ -76,14 +80,17 @@ def main():
     script_dir = Path(__file__).parent
     excel_file_path = script_dir.parent / 'Experimental Data' / 'Void Study FULL DOC.xlsx'
     excel_sheet = openpyxl.load_workbook(excel_file_path)
+    workbook_name = 'Photoshop Void Data (2)'
 
     for i, label in enumerate(pre_images_full.keys()):
-        excel_sheet['Photoshop Void Data'].cell(row=1, column=i+1).value = label
-        excel_sheet['Photoshop Void Data'].cell(row=2, column=i+1).value = calculate_void_percent(pre_images_full[label])
+        excel_sheet.create_sheet(title=workbook_name)
+        excel_sheet[f'{workbook_name}'].cell(row=1, column=i+1).value = label
+        excel_sheet[f'{workbook_name}'].cell(row=2, column=i+1).value = calculate_void_percent(pre_images_full[label], cut=0.8)
 
     for i, label in enumerate(post_images_full.keys()):
-        excel_sheet['Post-Cycle Photoshop Void Data'].cell(row=1, column=i+1).value = label
-        excel_sheet['Post-Cycle Photoshop Void Data'].cell(row=2, column=i+1).value = calculate_void_percent(post_images_full[label])
+        excel_sheet.create_sheet(title=f'Post-Cycle {workbook_name}')
+        excel_sheet[f'Post-Cycle {workbook_name}'].cell(row=1, column=i+1).value = label
+        excel_sheet[f'Post-Cycle {workbook_name}'].cell(row=2, column=i+1).value = calculate_void_percent(post_images_full[label], cut=0.8)
 
     excel_sheet.save(excel_file_path)
 
